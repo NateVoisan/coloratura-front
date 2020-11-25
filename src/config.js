8ead5e2d33e94c46c858640d51b17765771d9bcc
Nate Voisan <rufio96@gmail.com>
2020-11-25 12:10:11 -0800

    deployment commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://radiant-reaches-79711.herokuapp.com/`,
+    API_ENDPOINT: `https://radiant-reaches-79711.herokuapp.com`,
     TOKEN_KEY: 'coloratura-front-auth-token',
   }
